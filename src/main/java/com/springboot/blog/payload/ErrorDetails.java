8591fb8316f714e9f5b3395c4c1b0e4d358ef5be
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-06-01 22:25:33 +0600

    Exception Handling in Spring Boot App

---
+++ src/main/java/com/springboot/blog/payload/ErrorDetails.java
@@ -0,0 +1,15 @@
+package com.springboot.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+public class ErrorDetails {
+
+    private Date Timestamp;
+    private String message;
+    private String details;
+}
